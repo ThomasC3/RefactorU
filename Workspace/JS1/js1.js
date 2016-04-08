5c633e752897bd40299fd5501ae11953c8305fd3
thomasc3 <thomas.p.cal@gmail.com>
2016-04-08 21:18:21 +0000

    workspace

---
+++ Workspace/JS1/js1.js
@@ -0,0 +1,15 @@
+            function getValue(){
+               var retVal = prompt("Welcome! What is your name?: ", "your name here");
+               document.write("Hello " + retVal);
+            }
+            var name = prompt("Hello Boulder!");
+
+if (name.toLowerCase() == "name") {
+  alert("Welcome! name");
+}
+
+
+var name = window.prompt(); // open the blank prompt window
+var name = prompt();       //  open the blank prompt window
+var name = window.prompt('Hello Boulder'); // open the window with Text "Are you feeling lucky"
+var name = window.prompt('Hello Boulder', 'Whats your name?');
