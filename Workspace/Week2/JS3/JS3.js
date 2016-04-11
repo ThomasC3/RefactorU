856f67824fa5d80953dd13cb0e2f7dcb976d3ebe
thomasc3 <thomas.p.cal@gmail.com>
2016-04-11 19:05:48 +0000

    workspace

--- Workspace/Week2/JS3/JS3.js
+++ Workspace/Week2/JS3/JS3.js
@@ -1,2 +1,20 @@
+var phoneNumber = prompt('What is your phone number? (Dont worry, we hate spam too)');
 
-alert('Hello Boulder!');
+alert(phoneNumber.charAt(3)=== '-' && phoneNumber.charAt(7) === '-');
+
+var birthDate = prompt('What is your birthdate?');
+
+alert(birthDate.charAt(2)==='/' && birthDate.charAt(5) ==='/');
+
+var zipCode = prompt('What is your zip code?');
+
+alert(zipCode.length === 5 || (zipCode.charAt(5) === '-' && zipCode.length === 10));
+
+var state = prompt('What state are you from?');
+
+alert(/^[A-Z]/.test(state) && state.length === 2);
+
+var married = prompt('Are you married?');
+
+alert((married.toLowerCase()) === 'yes' || (married.toLowerCase() === 'no'));
+
