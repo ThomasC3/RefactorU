daa790893ea50de57c86d2b12eb159732f51d7d5
thomasc3 <thomas.p.cal@gmail.com>
2016-04-12 20:00:18 +0000

    workspace

--- Workspace/Week2/JS4 Victims & Volunteers/js4.js
+++ Workspace/Week2/JS4 Victims & Volunteers/js4.js
@@ -3,35 +3,34 @@ var field, fields = {
   'number': undefined,
   'street': undefined
 };
-
 var type, types = ['victim', 'volunteer'];
 
 var loopThrough = function (func) {
-  for (field in fields) {
-      func(field);
-  }
+    for (field in fields) {
+        func(field);
+    }
 };
 
 var getData = function(value, i) {
-  loopThrough(function(field){
-    fields[field] = prompt(value + ' ' + i + ' - ' + field + ':');
-  });
+    loopThrough(function(field){
+        fields[field] = prompt(value + ' ' + i + ' - ' + field + ':');
+        });
 };
 
-var displayData = function (list, value, i) {
-  var li = document.createElement('li');
-  var ul = document.createElement('ul');
-
-  loopThrough(function (field) {
-    if (field == 'name') {
-      li.textContent = fields[field];
-    } else {
-      var listElement;
-      listElement = document.createElement('li');
-      listElement.textContent =  field + ": " + fields[field];
-      ul.appendChild( listElement );
-    }
-  });
+var displayData = function(list, value, i) {
+    var li = document.createElement('li');
+    var ul = document.createElement('ul');
+    
+    loopThrough(function (field) {
+        if (field == 'name') {
+            li.textContent = fields[field];
+        } else {
+          var listElement;
+          listElement = document.createElement('li');
+          listElement.textContent = field + ": " + fields[field];
+          ul.appendChild( listElement );
+        }
+    });
 
   li.appendChild( ul );
   list.appendChild(li);
