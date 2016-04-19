b0758075beabb2f292319e12c467269207ed0143
thomasc3 <thomas.p.cal@gmail.com>
2016-04-19 22:10:41 +0000

    workspace

--- Review/Practice Apps/Gold Rush/GoldRush.js
+++ Review/Practice Apps/Gold Rush/GoldRush.js
@@ -1,28 +1,65 @@
-angular.module("app", [])
+angular.module('myApp',[])
+.controller('myctrl', ['$scope',myFunc]);
+function myFunc($scope){
+   $scope.doClick=function(){
+   	$scope.pins=[]
 
-var mainControllerFunc = function($scope){
-
-	$scope.markers = []
-	
-	$scope.addMarker = function(event){
-		$scope.markerBox = {
-			xposition: (event.pageX - 24),
-			yposition: (event.pageY - 24),
-			//comment: prompt('enter a comment'),
-			commentVisibility: false,
-		}
-		$scope.markers.push($scope.markerBox)
-		console.log($scope.markers)
-		
-	}
-	
-	$scope.removeMarker = function(index){
-		$scope.markers.splice(index,1) 
-	}
-
-	$scope.toggleComment = function(index) {
-		$scope.markers[index].commentVisibility = !$scope.markers[index].commentVisibility
-	}	
+// console.log('something')
+   $scope.dropper=function() {
+   	
+   $scope.x = event.clientX;
+   $scope.y = event.clientY;
+   // var offsetX = event.offsetX;
+   // var offsetY = event.offsetY;
+   pins.push({'x':$scope.x, 'y': $scope.y});
+   
+   console.log(x, y);
+   
+   //$scope.marker = {
+   //	'display':'inherit;'
+   }
+   	
+   }
 }
 
-angular.module("app").controller("mainController", ["$scope", mainControllerFunc])
+
+   // Here you forward the coordinates e.pageX, e
+// angular.module("app", [])
+
+// .controller("goldController",["$scope", troller]);
+
+// function troller($scope) {
+   
+//     $scope.coords = function(event){
+//     $scope.x = event.clientX;
+//     $scope.y = event.clientY;
+//     coords = "X coords: " + x + ", Y coords: " + y;
+//     console.log(coords)
+// 	}
+// };
+// } = function($scope){
+
+// 	$scope.markers = []
+	
+// 	$scope.addMarker = function(event){
+// 		$scope.markerBox = {
+// 			xposition: (event.pageX - 24),
+// 			yposition: (event.pageY - 24),
+// 			//comment: prompt('enter a comment'),
+// 			commentVisibility: false,
+// 		}
+// 		$scope.markers.push($scope.markerBox)
+// 		console.log($scope.markers)
+		
+// 	}
+	
+// 	$scope.removeMarker = function(index){
+// 		$scope.markers.splice(index,1) 
+// 	}
+
+// 	$scope.toggleComment = function(index) {
+// 		$scope.markers[index].commentVisibility = !$scope.markers[index].commentVisibility
+// 	}	
+// }
+
+// angular.module("app").controller("mainController", ["$scope", mainControllerFunc])
