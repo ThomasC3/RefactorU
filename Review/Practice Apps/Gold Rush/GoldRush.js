7719152402f96a605de16d23b7fdea9c05bc2154
thomasc3 <thomas.p.cal@gmail.com>
2016-04-22 22:55:55 +0000

    workspace

--- Review/Practice Apps/Gold Rush/GoldRush.js
+++ Review/Practice Apps/Gold Rush/GoldRush.js
@@ -1,33 +1,29 @@
-angular.module("goldRush", []);
 
-angular.module('goldRush')
-   .controller('troller', ["$scope", findGold]);
-      function findGold($scope) {
-         //array holds marker cordinates 
-         $scope.marker=[];
-         //stores note
-         
-         //addes mark
-         $scope.drop = function(){
-           var x=event.pageX;
-           var y=event.pageY;
-         //var note =prompt(' Add note', '');
-            // console.log(x,y);
-           
-            
-            
-               // $scope.submitNote = function () {
-                  //pushing cords into marker arrays
-                  $scope.marker.push({"x":event.pageX,"y":event.pageY})   
-                  console.log($scope.marker)
-                  
-                  // $scope.notes=!$scope.notes
-               
-            
-         
-         //removes mark
-         $scope.removeMarker = function(index, event) {
-      $scope.marker.splice(index, 1)
-      }
-   }
+var mainController = function($scope){
+    console.log('controller is up')
+    $scope.addingANote = false
+    $scope.clickSpot = [0,0]
+    $scope.markers = []
+    $scope.noteInput = ''
+
+    $scope.addMarker = function(event){
+        $scope.clickSpot = {
+            x : event.pageX / document.body.clientWidth * 100,
+            y : event.pageY / document.body.clientHeight * 100
+        }
+        $scope.markers.push($scope.clickSpot)
+        $scope.addingANote = true
+    }
+    $scope.addNote = function(){
+        $scope.markers[$scope.markers.length -1].note = $scope.noteInput
+        $scope.addingANote = false
+        $scope.noteInput = ''
+    }
+
+    $scope.removeMarker = function(index){
+        $scope.markers.splice(index, 1)
+    }
 }
+
+angular.module('app', [])
+    .controller('mainController', ['$scope', mainController])
