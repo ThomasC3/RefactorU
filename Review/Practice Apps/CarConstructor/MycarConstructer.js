d38b9b81ccb5d03ec2b3998f7c3f4394649c5436
thomasc3 <thomas.p.cal@gmail.com>
2016-04-25 21:41:59 +0000

    workspace

---
+++ Review/Practice Apps/CarConstructor/MycarConstructer.js
@@ -0,0 +1,22 @@
+var Car = function(maxSpeed, driver) {
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time) {
+        console.log(speed * time);
+    }
+
+
+    this.logDriver = function() {
+        console.log("driver name is" + this.driver);
+    }
+}
+    var myCar = new Car(70, "Paul Walker")
+    var myCar2 = new Car(75, "Ricky Bobby")
+    var myCar3 = new Car(80, "Speed Racer")
+    var myCar4 = new Car(85, "Dale Earnhardt")
+    var myCar5 = new Car(100, "Mario")
+    
+    myCar.drive(30,5);
+    myCar3.logDriver();
+
+    // JavaScript File
