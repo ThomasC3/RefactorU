875e35c574cf15f4e77217c49cd6bfdba5fe64b6
thomasc3 <thomas.p.cal@gmail.com>
2016-05-10 21:55:35 +0000

    workspace

--- Workspace/Week6/Luminosity/colorutil.js
+++ Workspace/Week6/Luminosity/colorutil.js
@@ -1,10 +1,9 @@
-var exports = module.exports = { 
-
+var exports = module.exports = {};
 var red = process.argv[2];
 var green = process.argv[3];
 var blue = process.argv[4];
 
-var luminosity = function(){
+exports.luminosity = function(){
 	return 0.2126*red + 0.7152*green + 0.0722*blue;
 	
 }
