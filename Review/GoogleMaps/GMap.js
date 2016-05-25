be9f9f3ae7c5c1d2cc07d2f20c65d26141de9b43
thomasc3 <thomas.p.cal@gmail.com>
2016-05-24 20:24:49 -0600

    workspace

--- Review/GoogleMaps/GMap.js
+++
@@ -1,5 +0,0 @@
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
-});
-// JavaScript File
--- Review/GoogleMaps/mapper.js
+++
@@ -1,9 +0,0 @@
-var mapOptions = {
-    center: new google.maps.Latlng(37.7831, -122.4039)
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-    
-
-
-
-// JavaScript File
